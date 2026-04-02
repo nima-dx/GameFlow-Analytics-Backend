89a3dc971b78988aa6a91dda4db36f547bebf6ae
Louise Dantas <louisermdantas@gmail.com>
2026-04-02 20:23:45 +0000

    big query

---
+++ bigquery/create_datasets.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA IF NOT EXISTS `le-wagon-data-atelier.raw_dataset`;
+
+CREATE SCHEMA IF NOT EXISTS `le-wagon-data-atelier.analytics_dataset`;
---
+++ bigquery/create_external_tables.sql
@@ -0,0 +1,5 @@
+CREATE OR REPLACE EXTERNAL TABLE `le-wagon-data-atelier.raw_dataset.leagues`
+OPTIONS (
+  format = 'PARQUET',
+  uris = ['gs://gameflow-ingestion-processed/leagues/*.parquet']
+);
